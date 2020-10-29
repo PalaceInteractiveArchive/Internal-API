103a0b2a558669e15c4a1456fe49640dce55e493
Marc DiFilippo <marc@marcdif.com>
2020-10-29 15:48:07 -0400

    Add character filter

--- src/components/Minecraft/Chat/service.ts
+++ src/components/Minecraft/Chat/service.ts
@@ -1,8 +1,12 @@
 import { IChatService } from "./interface";
 
+/* Link Check */
 const linkRegex = new RegExp('([a-zA-Z]+[://]+)?([a-zA-Z0-9\-]+(\\.|\\(dot\\)|\\(\\)|\\*))+([a-zA-Z]{2,18})(\\/[a-zA-Z0-9\\/.,?=&_-]+)?', 'g');
 const domainRegex = new RegExp('([a-zA-Z-]{1,63})(\\.|\\(dot\\)|\\(\\)|\\*)([a-zA-Z-]{1,63})(\\/[a-zA-Z\\.]*)?$');
 const allowedDomains: string[] = ['palace.network', 'thepalacemc.com', 'palnet.us'];
+
+/* Character Check */
+const allowedCharacters: string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "_", "+", "[", "]", "{", "}", "/", "\\", "?", "|", ",", ".", "<", ">", "`", "~", ";", ":", "'", "\"", "√", "˚", "≤", "≥", "™", "£", "¢", "∞", "•", " "];
 
 const ChatService: IChatService = {
 
@@ -57,8 +61,25 @@ const ChatService: IChatService = {
     // You have been temporarily muted for 30 seconds. If your message was incorrectly blocked, a staff member will remove the mute.
 
     characterCheck(message: string): string[] {
-        // return ['blocked character', ''];
-        return null;
+        message = message.toLowerCase();
+        var invalidCharacters: string = "";
+
+        for (var i = 0; i < message.length; i++) {
+            var char = message.charAt(i);
+            if (invalidCharacters.indexOf(char) == -1 && !allowedCharacters.includes(char)) {
+                if (invalidCharacters.length > 0) {
+                    invalidCharacters += ", " + char;
+                } else {
+                    invalidCharacters += char;
+                }
+            }
+        }
+
+        if (invalidCharacters.length == 0) {
+            return null;
+        } else {
+            return ['blocked character' + (invalidCharacters.length == 1 ? '' : 's'), invalidCharacters];
+        }
     }
 }
 
