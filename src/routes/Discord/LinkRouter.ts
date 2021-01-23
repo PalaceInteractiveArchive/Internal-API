0a71e8cc617ed61d1a146ed699c0eb0636f3111c
Flakenstine <isaac@palace.interactive>
2021-01-22 17:41:19 -0700

    Setup initial code for discord endpoints

---
+++ src/routes/Discord/LinkRouter.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+/**
+ * @constant {express.Router}
+ */
+const router: Router = Router();
+
+// router.get('/discord/link/:uuid');
+
+export default router;
---
+++ src/routes/Discord/index.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+/**
+ * @constant {express.router}
+ */
+const router: Router = Router();
+
+export default router;
