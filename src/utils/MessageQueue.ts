28e42de531d1e7056c4e617a00b9e6d74d92f941
Marc DiFilippo <marc@marcdif.com>
2021-02-03 15:39:15 -0500

    Handle errors

--- src/utils/MessageQueue.ts
+++ src/utils/MessageQueue.ts
@@ -32,77 +32,82 @@ export class MessageQueue {
                 console.log(' [X] Listening for chat analysis requests');
 
                 channel.consume(queue, function reply(msg) {
-                    if (msg.content) {
-                        console.log(" [x] %s", msg.content.toString());
-                        var packet = JSON.parse(msg.content.toString());
-                        if (packet.id && packet.id == 13 && packet.message) {
-                            var message = packet.message;
-                            console.log("Analysing message '" + message + "'...");
-                            var swear = ChatService.swearCheck(message);
-                            if (swear != null) {
-                                var filter: string = swear[0];
-                                var offendingText: string = swear[1];
+                    try {
+                        if (msg.content) {
+                            console.log(" [x] %s", msg.content.toString());
+                            var packet = JSON.parse(msg.content.toString());
+                            if (packet.id && packet.id == 13 && packet.message) {
+                                var message = packet.message;
+                                console.log("Analysing message '" + message + "'...");
+                                var swear = ChatService.swearCheck(message);
+                                if (swear != null) {
+                                    var filter: string = swear[0];
+                                    var offendingText: string = swear[1];
 
-                                var response = {
+                                    var response = {
+                                        id: 14,
+                                        requestId: packet.requestId,
+                                        okay: false,
+                                        filter_caught: filter,
+                                        offending_text: offendingText
+                                    }
+
+                                    channel.ack(msg);
+                                    channel.publish('proxy_direct', packet.sendingProxy, Buffer.from(JSON.stringify(response)));
+                                    return;
+                                }
+                                var link = ChatService.linkCheck(message);
+                                if (link != null) {
+                                    var filter: string = link[0];
+                                    var offendingText: string = link[1];
+
+                                    var response = {
+                                        id: 14,
+                                        requestId: packet.requestId,
+                                        okay: false,
+                                        filter_caught: filter,
+                                        offending_text: offendingText
+                                    }
+
+                                    channel.ack(msg);
+                                    channel.publish('proxy_direct', packet.sendingProxy, Buffer.from(JSON.stringify(response)));
+                                    return;
+                                }
+                                var character = ChatService.characterCheck(message);
+                                if (character != null) {
+                                    var filter: string = character[0];
+                                    var offendingText: string = character[1];
+
+                                    var response = {
+                                        id: 14,
+                                        requestId: packet.requestId,
+                                        okay: false,
+                                        filter_caught: filter,
+                                        offending_text: offendingText
+                                    }
+
+                                    channel.ack(msg);
+                                    channel.publish('proxy_direct', packet.sendingProxy, Buffer.from(JSON.stringify(response)));
+                                    return;
+                                }
+
+                                var response2 = {
                                     id: 14,
                                     requestId: packet.requestId,
-                                    okay: false,
-                                    filter_caught: filter,
-                                    offending_text: offendingText
+                                    okay: true,
+                                    message
                                 }
 
                                 channel.ack(msg);
-                                channel.publish('proxy_direct', packet.sendingProxy, Buffer.from(JSON.stringify(response)));
-                                return;
-                            }
-                            var link = ChatService.linkCheck(message);
-                            if (link != null) {
-                                var filter: string = link[0];
-                                var offendingText: string = link[1];
-
-                                var response = {
-                                    id: 14,
-                                    requestId: packet.requestId,
-                                    okay: false,
-                                    filter_caught: filter,
-                                    offending_text: offendingText
-                                }
-
+                                channel.publish('proxy_direct', packet.sendingProxy, Buffer.from(JSON.stringify(response2)));
+                            } else {
+                                console.log("Invalid packet: " + msg.content.toString());
                                 channel.ack(msg);
-                                channel.publish('proxy_direct', packet.sendingProxy, Buffer.from(JSON.stringify(response)));
-                                return;
                             }
-                            var character = ChatService.characterCheck(message);
-                            if (character != null) {
-                                var filter: string = character[0];
-                                var offendingText: string = character[1];
-
-                                var response = {
-                                    id: 14,
-                                    requestId: packet.requestId,
-                                    okay: false,
-                                    filter_caught: filter,
-                                    offending_text: offendingText
-                                }
-
-                                channel.ack(msg);
-                                channel.publish('proxy_direct', packet.sendingProxy, Buffer.from(JSON.stringify(response)));
-                                return;
-                            }
-
-                            var response2 = {
-                                id: 14,
-                                requestId: packet.requestId,
-                                okay: true,
-                                message
-                            }
-
-                            channel.ack(msg);
-                            channel.publish('proxy_direct', packet.sendingProxy, Buffer.from(JSON.stringify(response2)));
-                        } else {
-                            console.log("Invalid packet: " + msg.content.toString());
-                            channel.ack(msg);
                         }
+                    } catch (error) {
+                        console.log("Error handling message: " + error);
+                        channel.ack(msg);
                     }
                 }, { noAck: false });
             })
