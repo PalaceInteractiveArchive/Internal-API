1a0a951ade3bc744bdeb1b3eae7375435b3e0cc3
Flakenstine <isaacgoodwin97@gmail.com>
2021-03-25 21:12:22 -0600

    Cleanup code

--- src/components/Minecraft/index.ts
+++ src/components/Minecraft/index.ts
@@ -1,5 +1,6 @@
 import * as PacksComponent from './Packs';
 import * as ChatComponent from './Chat';
 import * as ServerComponent from './Server';
+import * as PlayerComponent from './Player';
 
-export { PacksComponent, ChatComponent, ServerComponent };
+export { PacksComponent, ChatComponent, ServerComponent, PlayerComponent };
