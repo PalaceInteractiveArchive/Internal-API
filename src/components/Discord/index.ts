ef98579ee0666b10d5368b55d37fed3a8fbd0500
Flakenstine <isaacgoodwin97@gmail.com>
2021-03-28 15:01:20 -0600

    Start working on linking process

---
+++ src/components/Discord/index.ts
@@ -0,0 +1,26 @@
+import Axios from "axios";
+import {Request, Response } from 'express';
+
+export const Link = async (req: Request, res: Response) => {
+    let client_id = '543141358496383048';
+    let client_secret = 'RI_ubQFraTuKXPS9JFuczgZdF--4RaUX'
+    let redirect_uri = 'https://internal-api.palace.network/discord/verify'
+
+    Axios.post('https://discord.com/api/oauth2/token', {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'authorization_code',
+        'code': req.params.code,
+        'redirect_uri': redirect_uri
+    })
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
+
+export const Verify = async (req: Request, res: Response) => {
+    res.send();
+}
---
+++ src/routes/Discord/LinkRouter.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const router: Router = Router();
+
+//routes will go here.
+
+export default router;
