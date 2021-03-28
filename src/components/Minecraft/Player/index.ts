21ef6e18c7f752cc2c015290ee66edc878ba1c0c
Flakenstine <isaacgoodwin97@gmail.com>
2021-03-28 12:56:41 -0600

    Added some auth to the route

--- src/components/Minecraft/Player/index.ts
+++ src/components/Minecraft/Player/index.ts
@@ -1,15 +1,24 @@
-import { mongoPlayer } from '@/components/Titan/User/model';
-import { Request, Response } from 'express';
+import * as HttpStatus from 'http-status-codes';
+import * as http from 'http';
 
-export const getPlayer = async (req: Request, res: Response):Promise<void> => {
-    await mongoPlayer.find({username: req.params.user}).lean().exec((err, results) => {
-        if (!results.length) {
-            res.send({message: 'Player Not Found!'})
-        } else {
-            let data = JSON.stringify(results[0]);
-            let parseData = JSON.parse(data);
-            let playerData = {uuid: parseData.uuid, username: parseData.username, rank: parseData.rank, onlineTime: parseData.onlineTime, tokens: parseData.tokens, balance: parseData.balance, tags: parseData.tags, server: parseData.server }
-            res.send(playerData);
-        }
-    })
+import { NextFunction, Request, Response } from 'express';
+import HttpError from '@/config/error';
+import { mongoPlayer } from '@/components/Titan/User/model';
+
+export const getPlayer = async (req: Request, res: Response, next: NextFunction):Promise<void> => {
+
+    if (!req.headers['service-api-key']) {
+        return next(new HttpError(HttpStatus.UNAUTHORIZED, http.STATUS_CODES[HttpStatus.UNAUTHORIZED]));
+    } else {
+        await mongoPlayer.find({username: req.params.user}).lean().exec((err, results) => {
+            if (!results.length) {
+                res.send({message: 'Player Not Found!'})
+            } else {
+                let data = JSON.stringify(results[0]);
+                let parseData = JSON.parse(data);
+                let playerData = {uuid: parseData.uuid, username: parseData.username, rank: parseData.rank, onlineTime: parseData.onlineTime, tokens: parseData.tokens, balance: parseData.balance, tags: parseData.tags, server: parseData.server }
+                res.send(playerData);
+            }
+        })
+    }
 }
