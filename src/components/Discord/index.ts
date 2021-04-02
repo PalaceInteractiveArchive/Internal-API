29404b575e4ae3dbf77017fd62fa8889f6509862
Flakenstine <isaacgoodwin97@gmail.com>
2021-04-02 16:42:02 -0600

    Trying to fix this method

--- src/components/Discord/index.ts
+++ src/components/Discord/index.ts
@@ -1,30 +1,31 @@
 import Axios from 'axios'
 import { Request, Response } from 'express'
 
-
 export const Link = async (req: Request, response: Response) => {
-  // const client_id = '543141358496383048';
+  const client_id = '543141358496383048';
   const client_secret = 'RI_ubQFraTuKXPS9JFuczgZdF--4RaUX';
   const redirect_uri = 'https://dev-internal-api.palace.network/discord/verify';
 
   let code = req.query.code;
-	// let data = {
-	// 	client_id: client_id,
-	// 	client_secret: client_secret,
-  //   code: code.toString(),
-	// 	scope: 'identify'
-	// }
-  const config = {
-    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+  let data = {
+    client_id: client_id,
+    client_secret: client_secret,
+    code: code.toString(),
+    grant_type: 'authorization_code',
+    scope: 'identify',
+    redirect_uri: redirect_uri,
   }
-  await Axios.post(`https://discord.com/api/oauth2/token?grant_type=authorization_code&client_id=543141358496383048&client_secret=${client_secret}&code=${code}&redirect_uri=${redirect_uri}&scope=identify`, config)
+  const config = {
+    headers: {'content-type': 'application/x-www-form-urlencoded'}
+  }
+  await Axios.post('https://discord.com/api/oauth2/token', new URLSearchParams(data), config)
     .then((res) => {
       let data = res.data;
       response.send(data);
     })
     .catch((err) => {
-      console.log(err)
-      response.status(500);
+      console.log(err);
+      response.sendStatus(500);
     })
 }
 
