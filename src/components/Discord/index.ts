8f5b9f71c169d3500fd5c1b4438d8c8ce74ef071
Flakenstine <isaacgoodwin97@gmail.com>
2021-04-04 20:05:52 -0600

    Added check to verify if user is apart of discord

--- src/components/Discord/index.ts
+++ src/components/Discord/index.ts
@@ -1,5 +1,7 @@
 import Axios from 'axios'
 import { Request, Response } from 'express'
+import { mongoDPlayer } from './model';
+import { BotServerCheck } from './service';
 
 export const Link = async (req: Request, response: Response) => {
   const client_id = '543141358496383048';
@@ -7,6 +9,7 @@ export const Link = async (req: Request, response: Response) => {
   const redirect_uri = 'https://dev-internal-api.palace.network/discord/link';
 
   let code = req.query.code;
+  let uuid = req.query.state;
   let data = {
     client_id: client_id,
     client_secret: client_secret,
@@ -21,24 +24,38 @@ export const Link = async (req: Request, response: Response) => {
   await Axios.post('https://discord.com/api/oauth2/token', new URLSearchParams(data), config)
     .then(async (res) => {
       let data = res.data;
+      await mongoDPlayer.findOneAndUpdate(
+        { uuid: uuid },
+        { $set: {'discord.access_token': data.access_token, 'discord.expires_in': data.expires_in, 'discord.refresh_token': data.refresh_token}})
       let config = {
         headers: {
           authorization: `${data.token_type} ${data.access_token}`
         }
       }
       await Axios.get('https://discord.com/api/users/@me', config)
-        .then((res) => {
+        .then(async (res) => {
           let data = res.data
-          console.log(data)
-          response.send(data)
+          await mongoDPlayer.findOneAndUpdate({uuid: uuid}, {$set: { 'discord.discordID': data.id}})
+          BotServerCheck(data.id, uuid)
+            .then(() => response.send(data));
         })
         .catch((err) => {
           console.log(err.data)
-          response.sendStatus(500)
+          response.status(500).send({message: 'Failed to fetch discord user data.'})
         })
     })
     .catch((err) => {
       console.log(err)
       response.sendStatus(500)
     })
+}
+
+export const Verify = async (req: Request, response: Response) => {
+  // const user = req.params.user;
+
+}
+
+
+export const Unlink = async (req: Request, response: Response) => {
+
 }
--- src/components/Discord/service.ts
+++ src/components/Discord/service.ts
@@ -1,6 +1,25 @@
-import amqp = require('amqplib/callback_api');
+import Axios from "axios"
+import { mongoDPlayer } from "./model";
 
-
-export const DiscordMQServer = () => {
-
+export const BotServerCheck = async (id: number, uuid: any) => {
+  const config = {
+    headers: {
+      Authorization: "Bot NTQzMTQxMzU4NDk2MzgzMDQ4.XFx9zg.8cxp7dBLDgjjvkrXyErfrDZqGeE"
+    }
+  }
+  await Axios.get(`https://discord.com/api/guilds/809995525210243072/members/${id}`, config)
+    .then(async (res) => {
+      let data = res.data;
+      if (!data) {
+        console.log('This user is not apart of our Discord server!');
+        await mongoDPlayer.findOneAndUpdate({ uuid: uuid }, { $unset: { 'discord': ""} }, () => {
+          console.log
+        })
+      } else {
+        console.log(data.user);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 }
