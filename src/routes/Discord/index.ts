42ce78cb24582fc7595a8a4f12ad201bbe16e1b0
Tom Roman <tom@tomroman.co.uk>
2021-05-10 18:38:33 +0100

    Fix non existent import

--- src/routes/Discord/index.ts
+++ src/routes/Discord/index.ts
@@ -1,10 +1,8 @@
-import { Link, Unlink} from "@/components/Discord";
+import { Link} from "@/components/Discord";
 import { Router } from "express";
 
 const router: Router = Router();
 
 router.get('/link', Link);
-
-router.get('/unlink/:uuid/:verify_code', Unlink)
 
 export default router;
